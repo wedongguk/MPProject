6f6fd1c0c4bdda12e23c18b0fccd161156976778
wedongguk <f4wedk@gmail.com>
2023-12-15 10:53:17 +0900

    MPProject

--- app/src/main/java/com/example/mp_project/WriteActivity.java
+++ app/src/main/java/com/example/mp_project/WriteActivity.java
@@ -3,20 +3,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CalendarView;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Calendar;
-import java.util.Date;
 
 public class WriteActivity extends AppCompatActivity{
     Button button;
@@ -35,35 +31,25 @@ public class WriteActivity extends AppCompatActivity{
         editText = findViewById(R.id.edit_text);
 
 
-        // datePicker를 현재날짜로 초기화해주기 위해 오늘의 년, 월, 일을 받아온다.
         Calendar cal = Calendar.getInstance();
         int year = cal.get(Calendar.YEAR);
         int month = cal.get(Calendar.MONTH);
         int day = cal.get(Calendar.DAY_OF_MONTH);
 
 
-        // datepicker를 오늘의 날짜로 초기값을 정해준다.
-        datePicker.init(year, month, day, new DatePicker.OnDateChangedListener() {
 
-            // datepicker에서 날짜가 바뀔때마다 파일이름을 정해준다
-            // readDiary메소드를 통해 파일이 존재하면 파일의 내용을 가져오고
-            // 그렇지 않다면 null을 가져온다.
+        datePicker.init(year, month, day, new DatePicker.OnDateChangedListener() {
             @Override
             public void onDateChanged(DatePicker datePicker, int i, int i1, int i2) {
 
                 filename = Integer.toString(i) + "_" + Integer.toString(i1+1) + "_" + Integer.toString(i2);
-                String str = readDiary(filename);
+                String str = diaryRead(filename);
                 editText.setText(str);
                 button.setEnabled(true);
 
 
             }
         });
-
-
-        //파일 생성, 쓰기단계
-        //editText에 적혀있는 내용의 byte값을 가져와
-        //파일에 쓰기한다.
 
         button.setOnClickListener(new Button.OnClickListener() {
             @Override
@@ -73,8 +59,8 @@ public class WriteActivity extends AppCompatActivity{
                     String str = editText.getText().toString();
                     outFs.write(str.getBytes());
                     outFs.close();
-                    Toast.makeText(WriteActivity.this, filename + "이 저장", Toast.LENGTH_SHORT).show();
-                    button.setText("수정하기");
+                    Toast.makeText(WriteActivity.this, filename + "일기 저장", Toast.LENGTH_SHORT).show();
+                    button.setText("수정");
 
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -87,21 +73,18 @@ public class WriteActivity extends AppCompatActivity{
 
     }
 
-    // 파일 읽기 단계
-    // 기록을 하지 않은 경우도 있으니 파일이 없는 경우도 있다.
-    // try catch를 통해 파일이 없는경우는 Hint를 띄워준다.
-    String readDiary(String filename){
+    String diaryRead(String filename){
 
         String diaryStr = null;
         FileInputStream inFs;
-        try {  //파일이 있는경우
+        try {
             inFs = openFileInput(filename);
 
             byte[] txt = new byte[500];
             inFs.read(txt);
             inFs.close();
             diaryStr = (new String(txt)).trim();
-            button.setText("수정하기");
+            button.setText("수정");
 
 
         }catch (IOException e){  // 파일이 없는 경우
